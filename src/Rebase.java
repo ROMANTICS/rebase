1796b8583873f67a6a63d5990071de4d0ee1213c
ROMANTICS <39127567+ROMANTICS@users.noreply.github.com>
2024-07-10 10:41:05 +0800

    rebase d

--- src/Rebase.java
+++ src/Rebase.java
@@ -1,5 +1,8 @@
 public class Rebase {
     public static void main(String[] args) {
-        System.out.println("rebase a");
+        System.out.println("rebase d");
+        System.out.println("rebase2 d");
+        System.out.println("rebase3 d");
+        System.out.println("rebase4 d");
     }
 }
