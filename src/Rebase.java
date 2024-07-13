46d6374b9fb164f71ecf0c8c70cb201f8bdcee2e
shixuemowang <3305322326@qq.com>
2024-07-13 23:50:33 +0800

    fix issue #2
    fix issue #2

--- src/Rebase.java
+++ src/Rebase.java
@@ -13,5 +13,6 @@ public class Rebase {
         System.out.println("dev");
         System.out.println("cleanup");
         System.out.println("master continue forward!");
+        System.out.println("issue #2 !");
     }
 }
